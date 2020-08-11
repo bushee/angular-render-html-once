fb3fee93e9f238e3b040a19cdea335b38adb678b
Krzystof "Bushee" Nowaczyk <bushee01@gmail.com>
2020-08-11 15:53:06 +0200

    <render-html-once> component implementation

--- src/lib/render-html-once.component.spec.ts
+++ src/lib/render-html-once.component.spec.ts
@@ -1,9 +1,78 @@
 import {RenderHtmlOnceComponent} from './render-html-once.component';
 
 describe('RenderHtmlOnceComponent', () => {
-  let component: RenderHtmlOnceComponent;
+    let hostElement: Element;
+    let component: RenderHtmlOnceComponent;
 
-  beforeEach(() => {
-    component = new RenderHtmlOnceComponent();
-  });
+    beforeEach(() => {
+        RenderHtmlOnceComponent.clearCache();
+        hostElement = document.createElement('div');
+        component = new RenderHtmlOnceComponent({nativeElement: hostElement});
+        component.id = 'component-id'
+    });
+
+    describe('ngOnInit()', () => {
+        it('should render passed html if it has not been rendered yet', () => {
+            // given
+            component.htmlContent =
+                '<div>hello world!</div>'
+                + '<span>another element</span>'
+                + 'some plain text here';
+            const expectedDivElement = document.createElement('div');
+            expectedDivElement.innerText = 'hello world!';
+            const expectedSpanElement = document.createElement('span');
+            expectedSpanElement.innerText = 'another element';
+            const expectedTextNode = document.createTextNode('some plain text here');
+
+            // when
+            component.ngOnInit();
+
+            // then
+            expect(hostElement.childNodes.length).toBe(3);
+            expect(hostElement.childNodes[0]).toEqual(expectedDivElement);
+            expect(hostElement.childNodes[1]).toEqual(expectedSpanElement);
+            expect(hostElement.childNodes[2]).toEqual(expectedTextNode);
+        });
+
+        it('should use previously rendered html if component is recreated', () => {
+            // given
+            component.htmlContent = '<div>original div element</div>';
+            component.ngOnInit();
+            const originalDivElement = hostElement.childNodes[0];
+
+            // and
+            const newHostElement = document.createElement('div');
+            const newComponent = new RenderHtmlOnceComponent({nativeElement: newHostElement});
+            newComponent.id = component.id;
+
+            // when
+            newComponent.ngOnInit();
+
+            // then
+            expect(hostElement.childNodes.length).toBe(0);
+            expect(newHostElement.childNodes.length).toBe(1);
+            expect(newHostElement.childNodes[0]).toBe(originalDivElement);
+        });
+
+        it('should use server-side rendered html if available', () => {
+            // given
+            const originalDivElement = document.createElement('div');
+            originalDivElement.textContent = 'original div element';
+            const serverSideRenderedComponent = document.createElement('render-html-once');
+            serverSideRenderedComponent.appendChild(originalDivElement);
+            serverSideRenderedComponent.id = component.id;
+            document.body.appendChild(serverSideRenderedComponent);
+            RenderHtmlOnceComponent.registerServerSideRenderedComponents();
+
+            // and
+            component.htmlContent = '<span><b>totally modified content</b></span>';
+
+            // when
+            component.ngOnInit();
+
+            // then
+            expect(hostElement.childNodes.length).toBe(1);
+            expect(hostElement.childNodes[0]).toBe(originalDivElement);
+        });
+    });
 });
--- src/lib/render-html-once.component.ts
+++ src/lib/render-html-once.component.ts
@@ -1,14 +1,38 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, ElementRef, Input, OnInit} from '@angular/core';
 
 @Component({
   selector: 'render-html-once',
   template: ''
 })
 export class RenderHtmlOnceComponent implements OnInit {
+    private static alreadyRendered: Record<string, Element> = {};
 
-  constructor() { }
+    @Input() public id: string;
+    @Input() public htmlContent: string;
 
-  ngOnInit() {
-  }
+    public constructor(private hostElement: ElementRef<Element>) {}
 
+    public ngOnInit(): void {
+        const cacheHit = RenderHtmlOnceComponent.alreadyRendered[this.id];
+        if (cacheHit) {
+            while (cacheHit.childNodes.length) {
+                this.hostElement.nativeElement.appendChild(cacheHit.firstChild);
+            }
+        } else {
+            this.hostElement.nativeElement.innerHTML = this.htmlContent;
+        }
+        RenderHtmlOnceComponent.alreadyRendered[this.id] = this.hostElement.nativeElement;
+    }
+
+    public static registerServerSideRenderedComponents(): void {
+        const elements = window.document.getElementsByTagName('render-html-once');
+        for (let i = 0; i < elements.length; ++i) {
+            const element = elements[i];
+            RenderHtmlOnceComponent.alreadyRendered[element.id] = element;
+        }
+    }
+
+    public static clearCache(): void {
+        RenderHtmlOnceComponent.alreadyRendered = {};
+    }
 }
